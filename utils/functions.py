6252669192713061540c0f314ed481b6b0a6f3fc
Toma0916 <yiyiaxpb@gmail.com>
2020-05-16 21:25:42 +0900

    datasetを実装 :art:

---
+++ utils/functions.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+from pathlib import Path
+import random
+import re
+import sys
+import gc
+import six
+import json
+import time
+import datetime
+from logging import getLogger
+from time import perf_counter
+import warnings
+import glob
+
+import numpy as np 
+from numpy.random.mtrand import RandomState
+import pandas as pd 
+
+from PIL import Image, ImageEnhance, ImageOps, ImageDraw
+import cv2
+
+import matplotlib.pyplot as plt
+
+# # --- torch ---
+# import torch
+# from torch import nn
+# import torch.nn.functional as F
+# from torch.nn import Sequential
+# from torch.autograd import Variable
+# from torch.utils.data.dataset import Dataset
+# from torch.utils.data.dataloader import DataLoader
+# from torch.utils.tensorboard import SummaryWriter
+
+# # --- models ---
+# from sklearn import preprocessing
+# from sklearn.model_selection import KFold, train_test_split
+# from skimage.transform import AffineTransform, warp
+# import sklearn.metrics
+
+# # --- albumentations ---
+# import albumentations as A
+# from albumentations.core.transforms_interface import DualTransform
+
+
+def convert_dataframe(dataframe):
+    """
+    convert dataframe format
+    bbox -> x, y, w, h
+    """
+    dataframe['x'] = -1
+    dataframe['y'] = -1
+    dataframe['w'] = -1
+    dataframe['h'] = -1
+
+    def expand_bbox(x):
+        r = np.array(re.findall("([0-9]+[.]?[0-9]*)", x))
+        if len(r) == 0:
+            r = [-1, -1, -1, -1]
+        return r
+
+    dataframe[['x', 'y', 'w', 'h']] = np.stack(dataframe['bbox'].apply(lambda x: expand_bbox(x)))
+    dataframe.drop(columns=['bbox'], inplace=True)
+    dataframe['x'] = dataframe['x'].astype(np.float)
+    dataframe['y'] = dataframe['y'].astype(np.float)
+    dataframe['w'] = dataframe['w'].astype(np.float)
+    dataframe['h'] = dataframe['h'].astype(np.float)
+    return dataframe
+
