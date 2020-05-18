9999ceab2f3dbbc470a0bba8065ff89f68291ce4
Toma0916 <yiyiaxpb@gmail.com>
2020-05-18 16:45:33 +0900

    Add some augmentation logic.

--- utils/transform.py
+++ utils/transform.py
@@ -45,15 +45,62 @@ from albumentations.pytorch.transforms import ToTensorV2
 
 
 class Transform:
+    """
+    https://github.com/katsura-jp/tour-of-albumentations 参考
+    """
 
     def __init__(self, config, is_train):
 
         if is_train or config['test_time_augment']:
-            self.blur_p =  config['blur_p']  # blutをかける確率
-            self.brightness_contrast_p =  config['brightness_contrast_p']  # brightness contrastを調整する確率
+
+            # flip系
+            self.horizontal_frip = config['horizontal_frip'] if ('horizontal_frip' in config) else {'p': 0.0}
+            self.vertical_frip = config['vertical_frip'] if ('vertical_frip' in config) else {'p': 0.0}
+
+            # blur系
+            self.blur = config['blur'] if ('blur' in config) else {'p': 0.0}
+            self.motion_blur = config['motion_blur'] if ('motion_blur' in config) else {'p': 0.0}
+            self.median_blur = config['median_blur'] if ('median_blur' in config) else {'p': 0.0}
+            self.gaussian_blur = config['gaussian_blur'] if ('gaussian_blur' in config) else {'p': 0.0}
+
+            # distortion系
+            self.optical_distorion = config['optical_distorion'] if ('optical_distorion' in config) else {'p': 0.0}
+            self.grid_distorion = config['grid_distorion'] if ('grid_distorion' in config) else {'p': 0.0}
+            self.elastic_distorion = config['elastic_distorion'] if ('elastic_distorion' in config) else {'p': 0.0}
+
+            # color系
+            self.clahe = config['clahe'] if ('clahe' in config) else {'p': 0.0}
+            self.chennel_shufflle = config['chennel_shufflle'] if ('chennel_shufflle' in config) else {'p': 0.0} 
+            self.random_gamma = config['random_gamma'] if ('random_gamma' in config) else {'p': 0.0}
+            self.hue_saturation_value = config['hue_saturation_value'] if ('hue_saturation_value' in config) else {'p': 0.0}
+            self.rgb_shift = config['rgb_shift'] if ('rgb_shift' in config) else {'p': 0.0}
+            self.random_brightness = config['random_brightness'] if ('random_brightness' in config) else {'p': 0.0}
+            self.random_contrast = config['random_contrast'] if ('random_contrast' in config) else {'p': 0.0}
+            
+            # noise系
+            self.gaussian_noise = config['gaussian_noise'] if ('gaussian_noise' in config) else {'p': 0.0}
+            self.cutout = config['cutout'] if ('cutout' in config) else {'p': 0.0}
+
+
         else:
-            self.blur_p = 0.0
-            self.brightness_contrast_p = 0.0
+            self.horizontal_frip = {'p': 0.0}
+            self.vertical_frip = {'p': 0.0} 
+            self.blur = {'p': 0.0}
+            self.motion_blur = {'p': 0.0}
+            self.median_blur = {'p': 0.0}
+            self.gaussian_blur = {'p': 0.0}
+            self.optical_distorion = {'p': 0.0}
+            self.grid_distorion = {'p': 0.0}
+            self.elastic_distorion = {'p': 0.0}
+            self.clahe = {'p': 0.0}
+            self.chennel_shufflle = {'p': 0.0}
+            self.random_gamma = {'p': 0.0}
+            self.hue_saturation_value = {'p': 0.0}
+            self.rgb_shift = {'p': 0.0}
+            self.random_brightness = {'p': 0.0}
+            self.random_contrast = {'p': 0.0}
+            self.gaussian_noise = {'p': 0.0}
+            self.cutout = {'p': 0.0}
 
     def __call__(self, example):
 
@@ -66,6 +113,24 @@ class Transform:
         }
 
         albumentation_transforms = A.Compose([
+            A.HorizontalFlip(**self.horizontal_frip),
+            A.VerticalFlip(**self.vertical_frip),
+            A.Blur(**self.blur),
+            A.MotionBlur(**self.motion_blur),
+            A.MedianBlur(**self.median_blur),
+            A.GaussianBlur(**self.gaussian_blur),
+            A.OpticalDistortion(**self.optical_distorion),
+            A.GridDistortion(**self.grid_distorion),
+            A.ElasticTransform(**self.elastic_distorion),
+            A.CLAHE(**self.clahe),
+            A.ChannelShuffle(**self.chennel_shufflle),
+            A.RandomGamma(**self.random_gamma),
+            A.HueSaturationValue(**self.hue_saturation_value),
+            A.RGBShift(**self.rgb_shift),
+            A.RandomBrightness(**self.random_brightness),
+            A.RandomContrast(**self.random_contrast),
+            A.GaussNoise(**self.gaussian_noise),
+            A.Cutout(**self.cutout),
             ToTensorV2(p=1.0)  # convert numpy image to tensor
             ], 
             bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
