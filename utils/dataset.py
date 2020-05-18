bb4a3bdb5f91226c7142818edecc77d40c800fdc
kminoda <koji.m.minoda@gmail.com>
2020-05-18 20:01:53 +0900

    Start implementing awesome augmentations

--- utils/dataset.py
+++ utils/dataset.py
@@ -43,6 +43,7 @@ import albumentations as A
 from albumentations.core.transforms_interface import DualTransform
 
 from utils.functions import convert_dataframe, filter_bboxes_by_size
+from utils.transform import Transform
 
 
 def collate_fn(batch):
@@ -98,49 +99,106 @@ class DatasetMixin(Dataset):
 
 class GWDDataset(DatasetMixin):
 
-    def __init__(self, dataframe, image_dir, transform=None, bbox_filter_config=None):
-        
+    def __init__(self, dataframe, image_dir, config=None, is_train=False):
+        self.transform_config = config['train']['augment']
+        self.bbox_filter_config = config['general']['bbox_filter']
+
+        transform = Transform(transform_config, is_train)
         super(GWDDataset, self).__init__(transform=transform)
 
         self.image_ids = dataframe['image_id'].unique()
         self.df = dataframe
         self.image_dir = image_dir
         self.indices = np.arange(len(self.image_ids))
-        self.bbox_filter_config = bbox_filter_config
+        self.mosaic = False
 
 
     def __len__(self):
         """return length of this dataset"""
         return len(self.indices)
-
     
     def get_example(self, i):
+        self.mosaic = True if np.random.rand() < self.transform_config['mosaic']['p'] else False
         image_id = self.image_ids[self.indices[i]]
-        records = self.df[self.df['image_id'] == image_id]
+        if self.mosaic:
+            img, targets = load_mosaic(self, i)
+        else:
+            records = self.df[self.df['image_id'] == image_id]
 
-        image = cv2.imread(str(self.image_dir/('%s.jpg' % image_id)), cv2.IMREAD_COLOR)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
-        image /= 255.0
+            image = cv2.imread(str(self.image_dir/('%s.jpg' % image_id)), cv2.IMREAD_COLOR)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+            image /= 255.0
 
-        boxes = records[['x', 'y', 'w', 'h']].values
-        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
-        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+            boxes = records[['x', 'y', 'w', 'h']].values
+            boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+            boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
 
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        area = torch.as_tensor(area, dtype=torch.float32)
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            area = torch.as_tensor(area, dtype=torch.float32)
 
-        labels = torch.ones((records.shape[0],), dtype=torch.int64)  # only one class (background or wheet)        
-        iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)  # suppose all instances are not crowd
+            labels = torch.ones((records.shape[0],), dtype=torch.int64)  # only one class (background or wheet)        
+            iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)  # suppose all instances are not crowd
 
-        target = {}
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['image_id'] = torch.tensor([self.indices[i]])
-        target['area'] = area
-        target['iscrowd'] = iscrowd
-        target = filter_bboxes_by_size(target, self.bbox_filter_config)
+            target = {}
+            target['boxes'] = boxes
+            target['labels'] = labels
+            target['image_id'] = torch.tensor([self.indices[i]])
+            target['area'] = area
+            target['iscrowd'] = iscrowd
+            target = filter_bboxes_by_size(target, self.bbox_filter_config)
         return image, target, image_id
 
+def load_image(self, index):
+    # loads 1 image from dataset, returns img, original hw, resized hw
+    image_id = self.image_ids[index]
+    image = cv2.imread(str(self.image_dir/('%s.jpg' % image_id)), cv2.IMREAD_COLOR)
+    h0, w0 = image.shape[:2]  # orig hw
+    return image, (h0, w0), image.shape[:2]  # img, hw_original, hw_resized
+
+def load_mosaic(self, index):
+    labels4 = []
+    s = self.img_size
+    xc, yc = [int(random.uniform(s * 0.5, s * 1.5)) for _ in range(2)]  # mosaic center x, y
+    indices = [index] + [random.randint(0, len(self.labels) - 1) for _ in range(3)]  # 3 additional image indices
+    for i, index in enumerate(indices):
+        # Load image
+        img, _, (h, w) = load_image(self, index)
+
+        # place img in img4
+        if i == 0:  # top left
+            img4 = np.full((s * 2, s * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
+            x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
+            x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
+        elif i == 1:  # top right
+            x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
+            x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
+        elif i == 2:  # bottom left
+            x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
+            x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
+        elif i == 3:  # bottom right
+            x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
+            x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
+
+        img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
+        padw = x1a - x1b
+        padh = y1a - y1b
+
+        # Labels
+        x = self.labels[index]
+        labels = x.copy()
+        if x.size > 0:  # Normalized xywh to pixel xyxy format
+            labels[:, 1] = w * (x[:, 1] - x[:, 3] / 2) + padw
+            labels[:, 2] = h * (x[:, 2] - x[:, 4] / 2) + padh
+            labels[:, 3] = w * (x[:, 1] + x[:, 3] / 2) + padw
+            labels[:, 4] = h * (x[:, 2] + x[:, 4] / 2) + padh
+        labels4.append(labels)
+
+    # Concat/clip labels
+    if len(labels4):
+        labels4 = np.concatenate(labels4, 0)
+        # np.clip(labels4[:, 1:] - s / 2, 0, s, out=labels4[:, 1:])  # use with center crop
+        np.clip(labels4[:, 1:], 0, 2 * s, out=labels4[:, 1:])  # use with random_affine
+    return img4, labels4
 
 if __name__ == '__main__':
 
