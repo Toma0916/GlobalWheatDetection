0b13f96f304519b308c74eee32e141d98dccf210
kminoda <koji.m.minoda@gmail.com>
2020-05-20 10:45:18 +0900

    added some values in class for debug

--- utils/dataset.py
+++ utils/dataset.py
@@ -100,7 +100,7 @@ class DatasetMixin(Dataset):
 class GWDDataset(DatasetMixin):
 
     def __init__(self, dataframe, image_dir, config=None, is_train=False):
-
+        self.config = config
         self.transform_config = config['train']['augment']
         self.bbox_filter_config = config['general']['bbox_filter']
         self.is_train = is_train
@@ -149,7 +149,9 @@ class GWDDataset(DatasetMixin):
         target['area'] = area
         target['iscrowd'] = iscrowd
 
-        if self.is_train or config['valid']['apply_bbox_filter']:
+        if not 'valid' in self.config.keys():
+            self.config['valid'] = {'apply_bbox_filter': False}
+        if self.is_train or self.config['valid']['apply_bbox_filter']:
             target = filter_bboxes_by_size(target, self.bbox_filter_config)
 
         # get another sample if number of bounding boxes is zero
