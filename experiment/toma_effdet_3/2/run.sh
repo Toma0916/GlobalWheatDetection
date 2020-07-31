992f8c9236c46915dc386b74c0282e954b447ff5
Toma0916 <yiyiaxpb@gmail.com>
2020-07-31 11:09:16 +0900

    Add experiments.

---
+++ experiment/toma_effdet_3/2/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="1"
+python train.py ./experiment/toma_effdet_3/2/config.json
---
+++ experiment/toma_effdet_3/4/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="3"
+python train.py ./experiment/toma_effdet_3/4/config.json
